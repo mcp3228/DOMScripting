b8ec8372c0f0a7fe18a2e55de9f4279365ffad63
duperweb <duperweb@gmail.com>
2018-04-15 14:06:47 -0400

    all my project

---
+++ js/index.js
@@ -0,0 +1,8 @@
+var menuIcon = document.getElementById("menu-icon");
+var menuClose = document.getElementById("menu-close")
+
+menuIcon.addEventListener("click", showOff);
+
+function showOff() {
+    console.log(menuIcon);
+}
