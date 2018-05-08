b8a7a1e21a214ac6a5ce87d309fd75e1566e2b63
duperweb <duperweb@gmail.com>
2018-05-07 22:39:54 -0400

    a lil bit responsive for mobile under 480px.

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,57 +1,67 @@
+// check time to put on nightmode
+window.addEventListener("load", changeElse);
+function changeElse() {
+    if (timeNow.getHours() >= 17) {
+        changeBack();
+    }
+}
 // targeting element by ID
-var header = document.getElementById("header");
-var footer = document.getElementById("pages-footer");
-var btn = document.getElementById("buttonmode");
-var wrapper =document.getElementById("wrapper");
-var tt = document.getElementById("time")
+const header = document.getElementById("header");
+const footer = document.getElementById("pages-footer");
+const btn = document.getElementById("buttonmode");
+const wrapper = document.getElementById("wrapper");
+const tt = document.getElementById("time")
 
 // other html collection , header
-var subHeading = document.getElementsByClassName("subheadline");
-var slen = subHeading.length;
-var para = document.getElementsByTagName("p");
+const subHeading = document.getElementsByClassName("subheadline");
+const slen = subHeading.length;
+const para = document.getElementsByTagName("p");
 
 // called the currnt time /sec
-setInterval(()=>{
+const timeNow = new Date();
+setInterval(() => {
     tt.innerHTML = new Date();
 }, 1000)
+
 
 // event to element targeted
 btn.addEventListener("click", changeBack)
 
- // lop through all the subheadthin
- // add en event to each
-for (var i = 0; i < slen; i++) {
+// lop through all the subheadthin
+// add en event to each
+for (let i = 0; i < slen; i++) {
     subHeading[i].addEventListener("click", showOn)
 }
 function showOn() {
     // this refer to the subheading clicked
-   if(this.nextElementSibling.className == "hide") {
-      this.nextElementSibling.className = "";
-   } else {
+    if (this.nextElementSibling.className == "hide") {
+
+        this.nextElementSibling.className = "";
+    } else {
         this.nextElementSibling.className = "hide";
-   }
+    }
 
 }
+
 // just change the background 
 function changeBack() {
-    if(wrapper.style.background == "" || btn.textContent == "nightmode") {
+    if (wrapper.style.background == "" || btn.textContent == "nightmode") {
         wrapper.style.background = "black";
         wrapper.style.color = "white";
         wrapper.children[0].children[0].style.color = "white";
         btn.textContent = "foreyes";
-    } else if ( btn.textContent == "foreyes") {
+    } else if (btn.textContent == "foreyes") {
         wrapper.style.background = "#9c4405c4";
         wrapper.style.color = "maroon";
         wrapper.children[0].children[0].style.color = "maroon";
-        
         this.style.background = "maroon";
         header.style.background = "maroon";
         footer.style.background = "maroon";
-        for (var i = 0; i < slen; i++) {
+        for (let i = 0; i < slen; i++) {
             subHeading[i].style.background = "maroon"
         }
         btn.textContent = "normal";
-       
+
     } else if (btn.textContent == "normal") {
         wrapper.style.background = "white";
         wrapper.style.color = "#545454";
@@ -59,17 +69,11 @@ function changeBack() {
         this.style.background = "rgb(43, 47, 58)";
         header.style.background = "rgb(43, 47, 58)";
         footer.style.background = "rgb(43, 47, 58)";
-        for (var i = 0; i < slen; i++) {
+        for (let i = 0; i < slen; i++) {
             subHeading[i].style.background = "rgb(43, 47, 58)"
         }
         btn.textContent = "nightmode";
-    } 
+    }
 }
-// check time to put on nightmode
-window.addEventListener("load", changeElse);
- function changeElse() {
-   if(timeNow.getHours() >= 17) {
-    changeBack();
-   }
- }
+
 
