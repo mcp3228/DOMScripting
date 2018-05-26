6f4e93aa95719ccd11305aa13623de8a387e6011
duperweb <duperweb@gmail.com>
2018-05-25 23:22:54 -0400

    adding some new function with script

--- js/index.js
+++ js/index.js
@@ -1,26 +1,34 @@
 const bio = document.getElementById("bio");
-const  menuIcon = document.getElementById("menu-icon");
-const  menuClose = document.getElementById("menu-close");
 const aboutme = document.getElementById("aboutme");
+const uiClose = document.querySelectorAll(".ui-close");
+var aside = document.getElementsByClassName("side-pub-ads-news")[0] ||
+            document.getElementsByClassName("side-pub-ads-news")[0];
 
-menuIcon.addEventListener("click", showOff);
-aboutme.addEventListener("click", showAbout);
 
-function showOff() {
-    if(menuClose.style.display == "none") {
-        menuClose.style.display = "block";
-        menuClose.style.backgroundColor = "#44394a";
-    } else {
-        menuClose.style.display = "none";
-    }
+for (var ii = 0; ii < uiClose.length; ii++) {
+    uiClose[ii].addEventListener("click", closeBox);
 }
 
+function closeBox() {
+    if (this.parentElement) {
+        this.parentElement.remove(this.parentElement);
+        if (!aside.children) {
+            showAbout();
+            this.parentElement.parentElement.appendChild(bio);
+        }
+    }
+} 
+aboutme.addEventListener("click", showAbout);
+
 function showAbout() {
-    if(bio.style.display == "") {
+    if (!aside.children.length) {
+        aside.appendChild(bio);
+    } 
+    if (bio.style.display == "") {
         bio.style.display = "none";
-    } else{
+    } else {
         bio.style.display = "";
     }
 }
 
-window.onload = showAbout();
+window.onload = showAbout(), closeBox();
